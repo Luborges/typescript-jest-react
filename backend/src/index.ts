2c4d97d7df2820435bc14b829f761a2aad407576
Lucas Borges <lucas.s.m.b@hotmail.com>
2020-02-04 17:55:49 -0300

    codigo inutil removido

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,10 +11,6 @@ const port = process.env.SERVER_PORT;
 
 const app = express();
 
-// Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
-
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
     // render the index template
